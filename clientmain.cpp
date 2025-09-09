438d2ae80413781a1905facd25714c7dfbe8a0e5
Shoruda <axelbergman@gmail.comgit init axel-dev-Atouch program.txttouch .gitignoregit addgit config --global user.email axelbergman@gmail.com>
2025-09-09 13:35:39 +0200

    TEST2

--- clientmain.cpp
+++ clientmain.cpp
@@ -2,13 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 /* You will to add includes here */
-/*edit*/
-/*TEST*/
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
 
-
+/*HEJ*/
 // Included to get the support library
 #include <calcLib.h>
 
